29de1dc5aff02d3acfdb07410737c04fc6f32fe8
Joe Pires <pires.joe.a@gmail.com>
2020-07-26 20:47:29 -0400

    fixed subtree bug

---
+++ Graphs and Trees/Is Subtree/isSubtree.py
@@ -0,0 +1,27 @@
+def isSubtree(node1, node2):
+    if node1 is None or node2 is None:
+        return False
+    sb1 = ''
+    orderedString1 = orderedString(node1, sb1)
+    orderedString2 = orderedString(node2, "")
+    return orderedString2 in orderedString1
+
+
+def orderedString(root, stringVal):
+    if root is None:
+        stringVal += "X"
+        return stringVal
+    
+    stringVal += str(root.data)
+    stringVal += ' '
+
+    return orderedString(root.left, stringVal)
+    return orderedString(root.right, stringVal)
+
+
+
+class BSTNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
